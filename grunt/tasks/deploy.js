5d44ff8d12c270c4796740ce605aca6712dd7a68
Kate Le <thuy.le@apiumtech.com>
2016-05-04 23:21:04 +0700

    change task

--- grunt/tasks/deploy.js
+++ grunt/tasks/deploy.js
@@ -2,7 +2,7 @@
  * Created by kevin on 2/9/15.
  */
 module.exports = function(grunt) {
-    grunt.registerTask('deploy:dev', [
+    grunt.registerTask('heroku', [
         'build:dev',
         'concurrent:dev-deploy'
     ]);
